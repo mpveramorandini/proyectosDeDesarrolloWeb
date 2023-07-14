9d35fecee79f2fcae301752a1f05bb3fff6b2553
Maria Paula Vera <mariapaulavera@hotmail.com>
2023-07-14 15:21:21 -0300

    ok

--- relojCuentaRegresiva/app.js
+++ relojCuentaRegresiva/app.js
@@ -30,7 +30,7 @@ const months = [
   let tempMonth = tempDate.getMonth();
   let tempDay = tempDate.getDate();
   // months are ZERO index based;
-  const futureDate = new Date(tempYear, tempMonth, tempDay + 5, 12, 30, 0);
+  const futureDate = new Date(tempYear + 2, tempMonth, tempDay + 5, 12, 30, 0);
   
   // let futureDate = new Date(2020, 3, 24, 11, 30, 0);
   
